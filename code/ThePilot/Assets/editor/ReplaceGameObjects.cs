85a6c922d13d16194466a0358115695937191017
Amal Khalil <amal.k.khalil@outlook.com>
2017-04-17 15:49:20 +0200

    Create prefabs for road

---
+++ code/ThePilot/Assets/editor/ReplaceGameObjects.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ReplaceGameObjects : ScriptableWizard
+{
+	public bool copyValues = true;
+	public GameObject NewType;
+	public GameObject [] parentObjs;
+
+	[MenuItem("Custom/Replace GameObjects")]
+
+
+	static void CreateWizard()
+	{
+		ScriptableWizard.DisplayWizard("Replace GameObjects", typeof(ReplaceGameObjects), "Replace");
+	}
+
+	void OnWizardCreate()
+	{
+		foreach (GameObject parentObj in parentObjs) {
+			replaceRoad (parentObj);
+		}
+	
+
+	}
+
+	void replaceRoad(GameObject parentObj)
+	{
+		Transform [] children = parentObj.GetComponentsInChildren<Transform>();
+		foreach (Transform t in children)
+		{
+			if(t.name.StartsWith("road_straight"))
+			{
+				GameObject go = t.gameObject;
+				GameObject newObject;
+				newObject = (GameObject)EditorUtility.InstantiatePrefab(NewType);
+				newObject.transform.position = go.transform.position;
+				newObject.transform.rotation = go.transform.rotation;
+				newObject.transform.parent = go.transform.parent;
+				newObject.name = go.name;
+				newObject.transform.localScale = go.transform.localScale;
+
+				DestroyImmediate(go);
+			}
+		}
+	}
+}
