dc40b2491e2f727b88d5bf70805acd295033d059
Amal Khalil <amal.k.khalil@outlook.com>
2017-04-18 18:57:18 +0200

    Digital Counter with diff color

--- code/ThePilot/Assets/Scripts/DigitController.cs
+++ code/ThePilot/Assets/Scripts/DigitController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class DigitController : MonoBehaviour {
 
 	public int number;
+	public Color color;
+
+	public enum Color {
+		Green, Yellow, Red
+	}
 
 	private Animator animator;
 	// Use this for initialization
@@ -15,5 +20,14 @@ public class DigitController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.animator.SetInteger ("number", number);
+
+		int stateValue = 1;
+
+		if (Color.Yellow.Equals (this.color)) {
+			stateValue = 2;
+		} else if (Color.Red.Equals (this.color)) {
+			stateValue = 3;
+		}
+		this.animator.SetInteger ("color", stateValue);
 	}
 }
